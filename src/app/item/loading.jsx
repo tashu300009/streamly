4101aa702923a981748bfd91ae91597e41ed3d62
tashu300009 <tashu3000092000@gmail.com>
2024-05-11 18:02:08 +0530

    fix: loading image

--- src/app/item/loading.jsx
+++ src/app/item/loading.jsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 export default function Loading({ height = 60, width = 60 }) {
   return (
     <div className="animate-spin mx-auto self-center" style={{ height, width }}>
-      <Image src="/loading.png" width={width} height={height} />
+      <Image src="/streamly/loading.png" width={width} height={height} />
     </div>
   );
 }

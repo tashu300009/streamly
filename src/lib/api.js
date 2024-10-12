eaa120166be0446c08cb8571ec0df711e2373e9c
tashu300009 <tashu3000092000@gmail.com>
2024-10-13 00:51:36 +0530

    Update api.js

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,9 +1,14 @@
-import axios from "./axios";
-
 async function makeAxiosCall({ url, method, data = {} }) {
   try {
-    const response = await axios.request({ url, params: data, method });
-    return response.data;
+    return await fetch(`https://api.themoviedb.org/${url}`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json", // Set default headers
+        "Cache-Control": "no-store",
+        Authorization:
+          "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1YjU5NjRkZTZhMGE4NDVjYTRkMDNhOTM2NzE2ZjRiZSIsInN1YiI6IjY2MzNjMWM3OTlkNWMzMDEyMzU2ZDI3MSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.aTgBazyBeZ-eKHBsYdsHlFZtpvmlluQxs9TCKeqx3ck", // Example: Set authorization header
+      },
+    }).then((res) => res.json());
   } catch (error) {
     console.error("Error fetching data:", error);
     return null;

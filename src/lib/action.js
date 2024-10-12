d470c68496f0dc686a413f910646a3c8af5b5fee
tashu300009 <tashu3000092000@gmail.com>
2024-10-13 00:51:20 +0530

    Update action.js

--- src/lib/action.js
+++ src/lib/action.js
@@ -1,10 +1,10 @@
 import makeAxiosCall from "./api";
+import queryString from "query-string";
 
 export const searchMovie = async ({ data }) =>
   makeAxiosCall({
     method: "GET",
-    url: "3/search/multi",
-    data,
+    url: `3/search/multi?${queryString.stringify(data)}`,
   });
 
 export const findMovieById = async ({ id }) =>
